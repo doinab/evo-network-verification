7b67466df67e90de9684c5ae4e4b334ed884fc59
Doina <doinabucur@rn-145-97-157-99.eduroam.rug.nl>
2013-09-24 15:20:01 +0200

    CTP-10n-m

---
+++ CTP/10n/CTP-evo-10n-m-div04/MultihopOscilloscope_evosv/runSimulationRedirect.py
@@ -0,0 +1,87 @@
+from TOSSIM import *
+from tinyos.tossim.TossimApp import *
+from random import *
+import sys
+
+#n = NescApp("TestNetwork", "app.xml")
+#t = Tossim(n.variables.variables())
+t = Tossim([])
+r = t.radio()
+
+import sys
+
+if len(sys.argv) < 3:
+	print "Usage: runSimulation.py TOPOLOGY_FILE EVENTS_FILE"
+	sys.exit()
+	
+topologyFile   = sys.argv[1]
+eventsFile     = sys.argv[2]
+
+f = open(topologyFile, "r")
+lines = f.readlines()
+for line in lines:
+  s = line.split()
+  if (len(s) > 0):
+    if s[0] == "gain":
+      r.add(int(s[1]), int(s[2]), float(s[3]))
+      print "Link ", s[1], " to ", s[2]
+
+noise = open("noise/meyer-short.txt", "r")
+lines = noise.readlines()
+for line in lines:
+  str = line.strip()
+  if (str != ""):
+    val = int(str)
+    for i in range(0, 10):
+      m = t.getNode(i);
+      m.addNoiseTraceReading(val)
+
+f = open(eventsFile, "r")
+lines = f.readlines()
+for line in lines:
+  s = line.split()
+  if (len(s) > 0):
+    if s[0] == "reboot_event":
+      nodeId = int(s[1].split('=')[1])
+      time = int(s[2].split('=')[1])
+      m = t.getNode(nodeId);
+      m.createNoiseModel();
+      m.bootAtTime(time)
+      print "Booting", nodeId, "at time", time
+
+#for i in range(0, 10):
+#  m = t.getNode(i);
+#  m.createNoiseModel();
+#  # time = randint(t.ticksPerSecond(), 10 * t.ticksPerSecond())
+#  time = i * t.ticksPerSecond()
+#  m.bootAtTime(time)
+#  print "Booting", i, "at time", time
+
+print "Starting simulation."
+
+fEnergy = open("log_energy.txt", "w")
+fRouting = open("log_routing.txt", "w")
+fApp = open("log_app.txt", "w")
+
+t.addChannel("ENERGY_HANDLER", fEnergy)
+t.addChannel("TreeRouting", fRouting)
+t.addChannel("App", fApp)
+
+#t.addChannel("Gain", fGain)
+#t.addChannel("Route", sys.stdout)
+#t.addChannel("Forwarder", sys.stdout)
+#t.addChannel("TestNetworkC", sys.stdout)
+#t.addChannel("AM", sys.stdout)
+#t.addChannel("PointerBug", sys.stdout)
+#t.addChannel("QueueC", sys.stdout)
+#t.addChannel("Traffic", sys.stdout)
+#t.addChannel("Acks", sys.stdout)
+
+while (t.time() < 200 * t.ticksPerSecond()):
+  t.runNextEvent()
+
+fEnergy.close()
+fRouting.close()
+fApp.close()
+
+print "Completed simulation."
